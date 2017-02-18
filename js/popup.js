630bc8f91da97aa6defee5d348ef9ef64d6f959b
Michael EYS <eys201203@yahoo.com>
2017-02-18 04:17:17 +0300

    Декоративные элементы. Не встали ровно. Но уже...

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,21 @@
-      var writeusOpen = document.querySelector(".contacts-btn");
-      var writeusForm = document.querySelector(".modal-writeus");
-      var writeusClose = writeusForm.querySelector(".modal-writeus-close");
+var writeusOpen = document.querySelector(".contacts-btn");
+var writeusForm = document.querySelector(".modal-writeus");
+var writeusClose = writeusForm.querySelector(".modal-writeus-close");
 
-      writeusOpen.addEventListener("click", function (event) {
-        event.preventDefault();
-        writeusForm.classList.add("modal-writeus-show");
-      });
+writeusOpen.addEventListener("click", function (event) {
+  event.preventDefault();
+  writeusForm.classList.add("modal-writeus-show");
+});
 
-      writeusClose.addEventListener("click", function (event) {
-        event.preventDefault();
-        writeusForm.classList.remove("modal-writeus-show");
-      });
+writeusClose.addEventListener("click", function (event) {
+  event.preventDefault();
+  writeusForm.classList.remove("modal-writeus-show");
+});
 
-      window.addEventListener("keydown", function (event) {
-        if (event.keyCode === 27) {
-          if (writeusForm.classList.contains("modal-writeus-show")) {
-            writeusForm.classList.remove("modal-writeus-show");
-          }
-        }
-      });
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode === 27) {
+    if (writeusForm.classList.contains("modal-writeus-show")) {
+      writeusForm.classList.remove("modal-writeus-show");
+    }
+  }
+});
